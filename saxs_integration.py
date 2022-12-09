30773ccd159bbb8c11e2f0fc428bb3b55a315540
Samuel Eyley <samuel.eyley@kuleuven.be>
2022-12-09 14:24:09 +0100

    corrected to do subtraction in 1D

--- saxs_integration.py
+++ saxs_integration.py
@@ -50,6 +50,13 @@ print("Sample transmission:"+str(round(transmission_sample,4)))
 temperature = str(round(file['scan']['data']['p62']['t95tempproglinkam']['eh.01']['temperature'][()],2))
 print("Sample temperature:"+temperature)
 
+
+data_1D = poni.integrate1d(file['scan']['data']['saxs_raw'][()][0]/transmission_sample,
+                           1000, filename=args.sample+"_"+temperature+'_1D.dat', correctSolidAngle=True,
+                                  method='csr', radial_range=(None), azimuth_range=(None),
+                                  unit="q_A^-1", mask=mask.data, normalization_factor=1.0,
+                                  metadata=None,error_model="poisson") 
+
 if args.background != None:
     bkg = h5py.File(args.background,'r')
     if args.noscaling == True:
@@ -62,40 +69,41 @@ if args.background != None:
         temperature_bkg = str(-1000)
     print("Background transmission:"+str(round(transmission_bkg,4)))
     print("Background temperature:"+temperature_bkg)
-    if args.empty != None:
-        empty = h5py.File(args.empty,'r')
-        if args.noscaling == True:
-            transmission_empty = 1
-        else:
-            transmission_empty = empty['scan']['data']['beamstop_2'][()]/empty['scan']['data']['ic1'][()]
-        data_cor = ((file['scan']['data']['saxs_raw'][()][0]/transmission_sample)-
-                    (empty['scan']['data']['saxs_raw'][()][0]/transmission_empty)
-                    ) - ((args.bkg_factor*bkg['scan']['data']['saxs_raw'][()][0]/transmission_bkg) -
-                         (empty['scan']['data']['saxs_raw'][()][0]/transmission_empty))
-    else:
-                         data_cor = (file['scan']['data']['saxs_raw'][()][0]/
-                                     transmission_sample) - (bkg['scan']['data']['saxs_raw'][()][0]/
-                                                             transmission_bkg)
-else:
-    if args.empty != None:
-        empty = h5py.File(args.empty,'r')
-        if args.noscaling == True:
-            transmission_empty = 1
-        else:
-            transmission_empty = empty['scan']['data']['beamstop_2'][()]/empty['scan']['data']['ic1'][()]
-        data_cor = (file['scan']['data']['saxs_raw'][()][0]/
-                    transmission_sample)-(empty['scan']['data']['saxs_raw'][()][0]/transmission_empty)
-    else:
-        data_cor = file['scan']['data']['saxs_raw'][()][0]/transmission_sample
-#data_cor = bkg['scan']['data']['saxs_raw'][()][0]
-
-data_1D = poni.integrate1d(data_cor, 1000, filename=args.sample+"_"+temperature+'_1D.dat', correctSolidAngle=True,
+    
+    bkg_1D = poni.integrate1d(bkg['scan']['data']['saxs_raw'][()][0]/transmission_bkg,
+                              1000, filename=args.sample+"_"+temperature+'_bkg1D.dat', correctSolidAngle=True,
                                   method='csr', radial_range=(None), azimuth_range=(None),
                                   unit="q_A^-1", mask=mask.data, normalization_factor=1.0,
                                   metadata=None,error_model="poisson") 
-
-fig, ax = plt.subplots(1,2,figsize=(25,10),constrained_layout=True)
-fig1 = ax[0].imshow(data_cor,norm=LogNorm(vmin=1e-1, vmax=10**3),cmap=plt.get_cmap('plasma'))
+else:
+    data_I = data_1D[1]
+    data_sig = abs(data_1D[2])
+if args.empty != None:
+    empty = h5py.File(args.empty,'r')
+    if args.noscaling == True:
+        transmission_empty = 1
+    else:
+        transmission_empty = empty['scan']['data']['beamstop_2'][()]/empty['scan']['data']['ic1'][()]
+    empty_1D = poni.integrate1d(empty['scan']['data']['saxs_raw'][()][0]/transmission_empty,
+                              1000, filename=args.sample+"_"+temperature+'_empty1D.dat', correctSolidAngle=True,
+                                  method='csr', radial_range=(None), azimuth_range=(None),
+                                  unit="q_A^-1", mask=mask.data, normalization_factor=1.0,
+                                  metadata=None,error_model="poisson")
+    if args.background != None:
+        data_I = (data_1D[1]-empty_1D[1])-(bkg_1D[1]-empty_1D[1])
+        data_sig = abs(data_1D[2])+abs(empty_1D[2])+abs(bkg_1D[2])+abs(empty_1D[2])
+    else:
+        data_I = data_1D[1]-empty_1D[1]
+        data_sig = abs(data_1D[2])+abs(empty_1D[2])
+else:
+    if args.background == None:
+        data_I = data_1D[1]
+        data_sig = abs(data_1D[2])
+    else:
+        data_I = data_1D[1]-bkg_1D[1]
+        data_sig = abs(data_1D[2])+abs(bkg_1D[2])
+fig, ax = plt.subplots(figsize=(25,10),constrained_layout=True)
+"""fig1 = ax[0].imshow(data_cor,norm=LogNorm(vmin=1e-1, vmax=10**3),cmap=plt.get_cmap('plasma'))
 # fig1 = ax[0].imshow(data_cor,norm=LogNorm(vmin=300, vmax=10**5),cmap=plt.get_cmap('plasma'))
 # create an axes on the right side of ax. The width of cax will be 5%
 # of ax and the padding between cax and ax will be fixed at 0.05 inch.
@@ -108,15 +116,16 @@ cbar.ax.tick_params(labelsize=25)
 ax[0].set_xlabel('pixel',fontsize=25)
 ax[0].set_ylabel('pixel',fontsize=25)
 ax[0].tick_params(labelsize=25)
-
-# fig2 = ax[1].plot(q, Intensity)
-fig2 = ax[1].plot(data_1D[0],data_1D[1])
-ax[1].set_xscale('log')
-ax[1].set_yscale('log')
-ax[1].set_xlabel(r'$Q\/[\AA^{-1}]$',fontsize=25)
-ax[1].set_ylabel('intensity [a.u.]',fontsize=25)
-# ax[1].set_xticks([0.01,0.1,0.4,0.6])
-ax[1].get_xaxis().set_major_formatter(ticker.ScalarFormatter())
-ax[1].tick_params(labelsize=25)
-#plt.show()
-plt.savefig(args.sample+"_"+temperature+".png", bbox_inches="tight", dpi=600)
+"""
+# fig2 = ax.plot(q, Intensity)
+#fig2 = ax.plot(data_1D[0],data_I)
+fig = ax.errorbar(data_1D[0],data_I,yerr=data_sig)
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel(r'$Q\/[\AA^{-1}]$',fontsize=25)
+ax.set_ylabel('intensity [a.u.]',fontsize=25)
+# ax.set_xticks([0.01,0.1,0.4,0.6])
+ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+ax.tick_params(labelsize=25)
+plt.show()
+#plt.savefig(args.sample+"_"+temperature+".png", bbox_inches="tight", dpi=600)
