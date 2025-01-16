926ed02d32bfef6d83acf9a941ff4054e889bb3d
Samuel Eyley <samuel.eyley@kuleuven.be>
2025-01-16 18:28:30 +0100

    move to argparse to allow use as script

--- src/smswaxs/xeuss_edf_h5_multi.py
+++ src/smswaxs/xeuss_edf_h5_multi.py
@@ -1,5 +1,5 @@
 """This script converts Xeuss edf files to HDF5 format for archival storage"""
-import sys
+import argparse as ap
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 import fabio
@@ -18,14 +18,18 @@ def process_edf_file(file_path: Path, hdf):
     for attr_name, attr_value in infile.header.items():
         dset.attrs[attr_name] = attr_value
 
-def add_edfs_to_hdf5(directory: str, hdf5_filename: str):
+def add_edfs_to_hdf5():
     """Find all edf files in all subdirectories of the specified
     path and add them to the hdf5"""
-    files = list(Path(directory).glob('**/*.edf'))
-    hdf = h5py.File(hdf5_filename,'w')
+    parser = ap.ArgumentParser(description="Convert Xeuss edf files to HDF5 format")
+    parser.add_argument("directory", help="Directory containing edf files")
+    parser.add_argument("hdf5_filename", help="Name of the HDF5 file to create")
+    args = parser.parse_args()
+    files = list(Path(args.directory).glob('**/*.edf'))
+    hdf = h5py.File(args.hdf5_filename,'w')
     with ThreadPoolExecutor() as executor:
         executor.map(lambda file: process_edf_file(file, hdf), files)
     hdf.close()
 
 if __name__ == '__main__':
-    add_edfs_to_hdf5(sys.argv[1],sys.argv[2])
+    add_edfs_to_hdf5()
