8e4d0484b2c7fbabdab6bbeb0a21f70e7e73d66b
Samuel Eyley <samuel.eyley@kuleuven.be>
2026-02-13 09:33:39 +0100

    fix: make topas_init into a package script

--- src/smswaxs/topas_init.py
+++ src/smswaxs/topas_init.py
@@ -6,13 +6,19 @@ Created on Wed Dec 20 11:38:05 2023
 """
 
 import sys
+def cli():
+    if len(sys.argv) != 2:
+        print("Usage: python topas_init.py <input_file>")
+        sys.exit(1)
+    with open(sys.argv[1],'r',encoding="utf-8") as inputfile:
+        data = inputfile.readlines()
 
-with open(sys.argv[1],'r',encoding="utf-8") as inputfile:
-    data = inputfile.readlines()
+    for i, line in enumerate(data):
+        if 'xdd' in line:
+            data[i] = 'xdd '+sys.argv[1][:-3]+'xye\n'
 
-for i, line in enumerate(data):
-    if 'xdd' in line:
-        data[i] = 'xdd '+sys.argv[1][:-3]+'xye\n'
+    with open(sys.argv[1], 'w',encoding="utf-8") as outfile:
+        outfile.writelines(data)
 
-with open(sys.argv[1], 'w',encoding="utf-8") as outfile:
-    outfile.writelines(data)
+if __name__ == "__main__":
+    cli()
